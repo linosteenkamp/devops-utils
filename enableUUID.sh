e031d228667b1f4f437ae19db074e2cbfaf7d1b2
Lino Steenkamp <lino.steenkamp@aax.co.za>
2018-04-30 08:57:16 +0200

    Initial commit

---
+++ enableUUID.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+input=${1}
+
+[ -z "${input}" ] && printf "provide virtual machine name or name pattern to match \r\n" && exit 1
+
+vms=$(govc ls vm | grep $input)
+
+for x in $vms; do 
+  echo "Modifying $x"
+  $(govc vm.change -e="disk.enableUUID=1" -vm="$x") 
+  $(govc vm.power -r "$x" >/dev/null 2>&1) 
+done 
